252fa0a8ab64f8bece00a300bda5cce8d4d789a7
Runit Ranjan <runitranjankumar@gmail.com>
2022-03-16 02:56:41 +0530

    initial_commit

--- app/urls.py
+++ app/urls.py
@@ -5,8 +5,6 @@ from . import views
 
 
 urlpatterns = [
-  #  path('', include('eventrr.urls')),
-
     path('admin/', admin.site.urls),
     path('students/',views.get_all_or_post,name='get_all_or_post'), # get or add a student
     path('students/<int:id>',views.get_or_update,name='get_or_update'), # UPDATE a student or get
--- app/views.py
+++ app/views.py
@@ -9,34 +9,67 @@ from django.db.models import F
 
 from django.views.decorators.csrf import csrf_exempt
 from django.core import serializers
+from datetime import datetime
 
-
-'''
-
-
-class Student(models.Model):
-    first_name = models.CharField(max_length=100)
-    last_name = models.CharField(max_length=100)
-    date_of_birth = models.DateField()
-    grade = models.IntegerField()
-    phone = models.CharField(max_length=100)
-    email = models.CharField(max_length=100)
-'''
 
 
 from .models import Student 
 # Create your views here.
 @csrf_exempt
-def student(request):
+def get_all_or_post(request):
   if request.method == 'GET':
-    stud_list = list(Student.objects.all().order_by('-id'))
+    stud_list = list(Student.objects.all().values('id','first_name','last_name','date_of_birth','grade','phone','email').order_by('id'))
     print(stud_list)
     #SomeModel_json = serializers.serialize("json", Student.objects.all())
+    return HttpResponse(stud_list,status=200)
+  elif request.method == 'POST':
 
-    return HttpResponse(stud_list)
+    first_name = request.POST.get('first_name')
+    last_name = request.POST.get('last_name')
+    date_of_birth = request.POST.get('date_of_birth')
+    grade = request.POST.get('grade')
+    phone = request.POST.get('phone')
+    email = request.POST.get('email')
 
-  '''
-    from django.http import JsonResponse
+    stud_ob = Student(first_name = first_name,last_name=last_name, date_of_birth = datetime.now() , grade = grade ,phone = phone,email=email)
+    stud_ob.save()
+    stud_ob = Student.objects.filter(first_name = first_name,last_name=last_name,grade = grade ,phone = phone,email=email).values('id','first_name','last_name','date_of_birth','grade','phone','email')
 
-    return JsonResponse(['a', 'b'], safe=False)
-  '''
+    print(stud_ob)
+    return HttpResponse(stud_ob,status=201)
+
+@csrf_exempt
+def get_or_update(request,id):
+  if request.method == 'GET':
+    try:
+      stud_ob = Student.objects.filter(id = id).values('id','first_name','last_name','date_of_birth','grade','phone','email')
+      #print(stud_ob)
+      return HttpResponse(stud_ob,status=200)
+    except:
+      return HttpResponse('',status=404)  
+
+    
+  elif request.method == 'POST':
+   
+    try:
+
+      stud_ob = Student.objects.get(id = id) #.values('id','first_name','last_name','date_of_birth','grade','phone','email')
+      first_name = request.POST.get('first_name')
+      last_name = request.POST.get('last_name')
+      date_of_birth = request.POST.get('date_of_birth')
+      grade = request.POST.get('grade')
+      phone = request.POST.get('phone')
+      email = request.POST.get('email')
+      stud_ob.first_name = first_name
+      stud_ob.last_name = last_name
+      #stud_ob.date_of_birth = date_of_birth
+      stud_ob.grade = grade
+      stud_ob.phone = phone
+      stud_ob.email = email
+      stud_ob.save()
+      stud_ob = Student.objects.filter(id = id).values('id','first_name','last_name','date_of_birth','grade','phone','email')
+      print(stud_ob)
+      return HttpResponse(stud_ob,status=200)
+    except:
+      return HttpResponse('some error',status=404)  
+    
